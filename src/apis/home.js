99d020b6f219ddc896d622d42b00413b390cc763
SLNY1921394421 <1921394421@qq.com>
2023-07-21 15:01:29 +0800

    Banner

--- src/apis/home.js
+++ src/apis/home.js
@@ -4,9 +4,13 @@ import httpInstance from "@/utils/http"
  * @param {*}
  * @return {*}
  */
-export const bannerAPI = () => {
+export const bannerAPI = (params = {}) => {
+  const { distributionSite = '1' } = params
   return httpInstance({
     url: 'home/banner',
+    params: {
+      distributionSite
+    }
   })
 }
 
