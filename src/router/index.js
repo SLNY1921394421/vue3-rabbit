bbe706c02c0f5c345191f0dbc7972c38086bfbe4
SLNY1921394421 <1921394421@qq.com>
2023-07-21 16:15:35 +0800

    二级分类----配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
 import Layout from '@/views/layout/index.vue'
 import Login from '@/views/login/index.vue'
+import SubCategory from '@/views/subCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,12 @@ const router = createRouter({
           path: 'category/:id',
           name: 'category',
           component: Category
-        }
+        },
+        {
+          path: 'category/sub/:id',
+          name: 'subCategory',
+          component: SubCategory,
+        },
       ]
     }, {
       path: '/Login',
