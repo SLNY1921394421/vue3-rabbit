b4009e890145baca76256b7446937952c505b201
SLNY1921394421 <1921394421@qq.com>
2023-07-22 09:28:34 +0800

    详情----大图显示

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -2,14 +2,21 @@
 import { ref, watch } from "vue"
 import { useMouseInElement } from '@vueuse/core'
 
+defineProps({
+  imageList: {
+    type: Array,
+    default: () => {}
+  }
+})
+
 // 图片列表
-const imageList = [
+/* const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
   "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
   "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+] */
 
 // 1.小图切换大图显示
 const activeIndex = ref(0)
@@ -57,7 +64,7 @@ watch([ elementX, elementY, isOutside ], () => {
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
-      <img :src="imageList[0]" alt="" />
+      <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
       <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
