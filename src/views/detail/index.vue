37db2f36a8c6b37b2101cd595033db4c3f3d5071
SLNY1921394421 <1921394421@qq.com>
2023-07-22 08:16:05 +0800

    详情---小图切换大图显示

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import { getDetail } from '@/apis/detail'
 import { useRoute } from "vue-router";
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 const route = useRoute()
 
 const goods = ref({})
@@ -39,7 +40,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
